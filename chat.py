4d99d40de2997a0587bba749108f962ee7bbbcbf
Randu Retana <randu2002@hotmail.com>
2023-05-15 12:48:16 -0600

    chatbot completo listo

--- chat.py
+++ chat.py
@@ -1,31 +1,12 @@
 import random
 import json
 import torch
-from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
 from database import save_user_data
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as f:
-    intents = json.load(f)
-
-FILE = "data.pth"
-data = torch.load(FILE)
-
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
 chat_state = "initial"
 user_data = {}
 
-
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
 
 awaiting_name_email = False
 
